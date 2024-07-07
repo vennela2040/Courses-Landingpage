d87d9765c77e8de8495811b8713ffec9d47322a0
vennela2040 <kudumulavennela@gmail.com>
2024-07-07 21:06:31 +0530

    integrated all sections

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,13 @@
 import Home from "../Components/Home"
 import InstructorsSlider from "../Components/InstructorsSlider"
 import NavBar from "../Components/NavBar"
+import ProgramOverview from "../Components/ProgramOverview"
+import Syllabus from "../Components/Syllabus"
+import Review from "../Components/Review"
+import Fee from "../Components/Fee"
+import InfoSection from "../Components/InfoSection"
+import Footer from "../Components/Footer"
+
 
 
 function App() {
@@ -8,7 +15,13 @@ function App() {
     <>
       <NavBar />
       <Home />
+      <ProgramOverview />
+      <Syllabus />
       <InstructorsSlider />
+      <Review />
+      <Fee />
+      <InfoSection />
+      <Footer />
     </>
   )
 }
